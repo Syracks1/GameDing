64758de76b87c6bb1efee53e05519febf965c5e7
Syracks1 <36195834+Syracks1@users.noreply.github.com>
2018-09-26 13:46:30 +0200

    Add project files.

---
+++ WpfApp1/Pages/CharCreate.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace WpfApp1.Pages
+{
+    /// <summary>
+    /// Interaction logic for Page1.xaml
+    /// </summary>
+    public partial class CharCreate : Page
+    {
+        public CharCreate()
+        {
+            InitializeComponent();
+        }
+
+        private void Confirm_Click(object sender, RoutedEventArgs e)
+        {
+            var NameTB = tb_name;
+            var RaceTB = cb_race;
+            var PowerTB = cb_power;
+            var ProfTB = cb_profession;
+
+            string Name = NameTB.Text;
+            string Race = RaceTB.Text;
+            string Power = PowerTB.Text;
+            string Prof = ProfTB.Text;
+
+            //var query = "INSERT INTO Character(Name, Race, Power, Prof) VALUES (Name, Race, Power, Prof);";
+            //var cmd = new SqlCommand(query);
+            //cmd.Parameters.AddWithValue("Name", Name);
+
+            string connectionString = null;
+            string sql = null;
+            //String connString = ConfigurationManager.ConnectionStrings["connWebOrdering"].ConnectionString;
+            //SqlConnection sqlConn = new SqlConnection(connString);
+            connectionString = "Data Source=.\\SQLEXPRESS;Initial Catalog=GameDing;Integrated Security=True";
+            using (SqlConnection cnn = new SqlConnection(connectionString))
+            {
+                cnn.Open();
+                sql = "insert into Character ([Name], [Race], [Power], [Prof]) values(@name,@race,@power,@prof)";
+                
+                using (SqlCommand cmd = new SqlCommand(sql, cnn))
+                {
+                    cmd.Parameters.AddWithValue("@name", Name);
+                    cmd.Parameters.AddWithValue("@race", Race);
+                    cmd.Parameters.AddWithValue("@power", Power);
+                    cmd.Parameters.AddWithValue("@prof", Prof);
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("Row inserted !! ");
+                }
+            }
+        }
+
+        private void Cancel_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
